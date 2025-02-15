0696b746a131000fb6dc68e7d6568c2ac8a941ce
kerimturak <kerimturak@homail.com>
2025-02-16 00:57:51 +0300

    pattern update

--- 003-pattern_detect/rtl/pattern_detector.sv
+++ 003-pattern_detect/rtl/pattern_detector.sv
@@ -13,10 +13,10 @@ output logic out);
   always_comb begin
     
     case(state)
-      S0: next_state = data ? S1 : S0;
-      S1: next_state = data ? S0 : S2;
-      S2: next_state = data ? S0 : S3;
-      S3: next_state = data ? S0 : S0;
+      S0: next_state = data ? S1 : S0; // 1 -> go next 0 -> stay here
+      S1: next_state = data ? S1 : S2; // 0 -> go next 1 -> stay here
+      S2: next_state = data ? S1 : S3; // 0 -> go next 1 -> go S1
+      S3: next_state = S0;             // 1 -> go S0     -> go S0
     endcase
     
     out = state == S3 && data == 1'b1;
--- 003-pattern_detect/rtl/pattern_if.sv
+++ 003-pattern_detect/rtl/pattern_if.sv
@@ -1,4 +1,12 @@
 interface pattern_if(input clk, rst);
   logic data;
   logic out;
+  
+  property PRO_pattern;
+    @(posedge clk)
+    $rose(data) ##1 $fell(data) ##1 data == 0 ##1 $rose(data);
+  endproperty
+
+  PATTERN: assert property(PRO_pattern) $display("pattern is found");
+    
 endinterface
